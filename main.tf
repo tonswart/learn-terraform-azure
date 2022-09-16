4e56586fd35cabf49777dea6e503637e00d2a186
Ton Swart <swart.ton@gmail.com>
2022-09-16 16:18:39 +0200

    Added iamge version

--- main.tf
+++ main.tf
@@ -19,10 +19,10 @@ provider "azurerm" {
   features {}
 }
 
-# variable "imagebuild" {
-#   type        = string
-#   description = "Latest Image Build"
-# }
+variable "imagebuild" {
+  type        = string
+  description = "Latest Image Build"
+}
 
 resource "azurerm_resource_group" "tf_test" {
   name     = "tfmainrg"
@@ -40,7 +40,7 @@ resource "azurerm_container_group" "tfcg_test" {
 
   container {
     name   = "weatherapi"
-    image  = "tonswart/weatherapi"
+    image  = "tonswart/weatherapi:${var.imagebuild}"
     cpu    = 1
     memory = 1
 
